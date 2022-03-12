4af64506a70424e3dcde750a3425399738357004
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-03-12 18:28:16 +0100

    up to date commit

--- simple_signing_plugin/CHANGELOG.md
+++ simple_signing_plugin/CHANGELOG.md
@@ -1,2 +1,6 @@
 ## 0.0.1
 Initial release
+## 0.0.2
+Updates to improve scoring
+## 0.0.3
+Provided documentation
--- simple_signing_plugin/lib/simple_signing_plugin.dart
+++ simple_signing_plugin/lib/simple_signing_plugin.dart
@@ -2,22 +2,25 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+///Main class with methods to sign and verify data and to check if the device is secured with a screen lock.
 class SimpleSigningPlugin {
+  ///Dart MethodChannel to connect native code necessary for keys and authentication to Dart platform.
   static const MethodChannel _channel = MethodChannel('simple_signing_plugin');
 
+  ///Signs the provided string using natively generated RSA key.
+  ///In order to work correctly, a secure screen lock has to be set up (this can be checked with checkIfDeviceSecure()).
+  ///Returns signed data as "signature:data" if signing succeeds or false. An asynchronous function, has to be awaited.
   static Future<dynamic> signData(String data) async {
-    try {
-      var result = await _channel.invokeMethod('signData', {'data': data});
-      if (result != false) {
-        return result;
-      } else {
-        return false;
-      }
-    } on PlatformException {
+    var result = await _channel.invokeMethod('signData', {'data': data});
+    if (result != false) {
+      return result;
+    } else {
       return false;
     }
   }
 
+  ///Verifies provided data with local certificate. Returns true if the signature is valid and false if it's not.
+  ///An asynchronous function, has to be awaited.
   static Future<bool> verifyData(String data) async {
     var result = await _channel.invokeMethod('verifyData', {'data': data});
     if (result == true) {
@@ -27,6 +30,7 @@ class SimpleSigningPlugin {
     }
   }
 
+  ///Checks if the screen lock has been set on the device. Returns true if it is set and false if not. An asynchronous function, has to be awaited.
   static Future<bool> checkIfDeviceSecure() async {
     var result = await _channel.invokeMethod('checkIfDeviceSecure');
     if (result == true) {
